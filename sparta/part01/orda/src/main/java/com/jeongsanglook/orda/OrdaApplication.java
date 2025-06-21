334ff72e977c114d035f4d27ef7e00b7c0cb7e25
kangboryung <boryung@boryung-MacBookPro.local>
2025-06-21 14:28:52 +0900

    first commit - 환경설정 및 create table sql 추가

---
+++ sparta/part01/orda/src/main/java/com/jeongsanglook/orda/OrdaApplication.java
@@ -0,0 +1,13 @@
+package com.jeongsanglook.orda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdaApplication.class, args);
+	}
+
+}
