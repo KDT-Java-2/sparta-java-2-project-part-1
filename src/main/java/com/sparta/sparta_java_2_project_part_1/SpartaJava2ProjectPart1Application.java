a95a43708f5fee6c6b8078355b7f0254dcebe37b
katiekim17 <pyeongsuk.kim88@gmail.com>
2025-06-21 10:52:56 +0900

    Initial commit

---
+++ src/main/java/com/sparta/sparta_java_2_project_part_1/SpartaJava2ProjectPart1Application.java
@@ -0,0 +1,13 @@
+package com.sparta.sparta_java_2_project_part_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaJava2ProjectPart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpartaJava2ProjectPart1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/sparta_java_2_project_part_1/SpartaJava2ProjectPart1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.sparta_java_2_project_part_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaJava2ProjectPart1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
