0c43f456e9970e81ca1aa548d98e569fca0425ff
jemmillion <whduswo94@gmail.com>
2025-06-21 13:42:06 +0900

    init commit

---
+++ src/main/java/com/sparta/java_practice_01/JavaPractice01Application.java
@@ -0,0 +1,13 @@
+package com.sparta.java_practice_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPractice01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPractice01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/java_practice_01/JavaPractice01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.java_practice_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPractice01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
