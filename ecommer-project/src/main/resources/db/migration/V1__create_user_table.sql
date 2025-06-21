f589d38bc0ca14f0529176ee726aeaa3b749ce91
tommyheo <jyhuh8775@naver.com>
2025-06-21 18:47:30 +0900

    fix: V1__create_user_table.sql 수정

--- ecommer-project/src/main/resources/db/migration/V1__create_user_table.sql
+++ ecommer-project/src/main/resources/db/migration/V1__create_user_table.sql
@@ -1,13 +1,23 @@
 CREATE TABLE user (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    -- uuid
+    alias VARCHAR(36) UNIQUE NOT NULL,
 
-    username VARCHAR(200) NOT NULL ,
+    -- 아이디
+    username VARCHAR(200) UNIQUE NOT NULL ,
+    -- 해싱된 패스워드
     password_hash VARCHAR(200) NOT NULL,
-    email VARCHAR(200) NOT NULL ,
+    -- 이메일
+    email VARCHAR(200) UNIQUE  NOT NULL ,
+    -- 이름
     name VARCHAR(50) NOT NULL,
+    -- 생년월일
     birth DATETIME NOT NULL,
-    phone_number VARCHAR(13) NOT NULL ,
+    -- 연락처
+    phone_number VARCHAR(13) NOT NULL,
+    -- 주소
     address VARCHAR(200) NOT NULL,
+    -- 역할
     role VARCHAR(8) NOT NULL ,
 
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
