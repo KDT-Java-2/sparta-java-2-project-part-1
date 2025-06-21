f0aba48ae5b3996c2ec86ae26f8a98502563bf06
techhan <devhan9117@gmail.com>
2025-06-21 14:44:50 +0900

    Cart flyway, entity 작성

---
+++ dogdog/src/main/java/com/dogworld/dogdog/domain/cart/Cart.java
@@ -0,0 +1,36 @@
+package com.dogworld.dogdog.domain.cart;
+
+import com.dogworld.dogdog.domain.BaseEntity;
+import com.dogworld.dogdog.domain.member.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Cart extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id", nullable = false)
+  private Member member;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private CartStatus status;
+
+}
