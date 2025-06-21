dfd4787b321ec95117db6111f10d8d1a02857e81
myeong-han <ames11118@gmail.com>
2025-06-21 12:18:12 +0900

    add Entities (User, Purchase, PurchaseProduct, Product, Category)

---
+++ src/main/java/com/sparta/java2/project/part1/commerce/common/enums/PurchaseStatus.java
@@ -0,0 +1,7 @@
+package com.sparta.java2.project.part1.commerce.common.enums;
+
+public enum PurchaseStatus {
+    PENDING,
+    COMPLETION,
+    CANCELLED,
+}
---
+++ src/main/java/com/sparta/java2/project/part1/commerce/domain/category/entity/Category.java
@@ -0,0 +1,38 @@
+package com.sparta.java2.project.part1.commerce.domain.category.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.sparta.java2.project.part1.commerce.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+
+@Entity
+@Table
+@DynamicInsert
+@DynamicUpdate
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Category extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false)
+    String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    @JsonBackReference
+    Category parent;
+
+    @Builder
+    public Category(String name, Category parent) {
+        this.name = name;
+        this.parent = parent;
+    }
+}
