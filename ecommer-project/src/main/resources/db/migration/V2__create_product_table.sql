cfd53f3957a32ed2f570053452ab38b935b7de55
tommyheo <jyhuh8775@naver.com>
2025-06-21 18:47:56 +0900

    fix: V2__create_product_table.sql 수정

--- ecommer-project/src/main/resources/db/migration/V2__create_product_table.sql
+++ ecommer-project/src/main/resources/db/migration/V2__create_product_table.sql
@@ -1,11 +1,20 @@
 CREATE TABLE product(
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    -- uuid
+    alias VARCHAR(36) UNIQUE NOT NULL,
 
+    -- 상품이름
     name VARCHAR(200) NOT NULL,
+    -- 상품 설명
     description TEXT NOT NULL,
+    -- 상품 가격
     price DECIMAL NOT NULL ,
+    -- 상품 재고
     stock int DEFAULT 0,
-    publisher BIGINT,
+    -- 상품 등록자
+    user_id BIGINT,
+    -- 카테고리
+    category_id BIGINT,
 
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME NULL ON UPDATE CURRENT_TIMESTAMP
