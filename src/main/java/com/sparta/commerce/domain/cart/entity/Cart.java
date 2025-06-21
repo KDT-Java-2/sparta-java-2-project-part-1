656c06f928e91f600f13eff741e82b695ae11dec
kim-yohan <jkim6115@gmail.com>
2025-06-21 13:27:44 +0900

    init

---
+++ src/main/java/com/sparta/commerce/domain/cart/entity/Cart.java
@@ -0,0 +1,60 @@
+package com.sparta.commerce.domain.cart.entity;
+
+import com.sparta.commerce.domain.product.entity.Product;
+import com.sparta.commerce.domain.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Table
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Getter
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Cart {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  User user;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "product_id")
+  Product product;
+
+  @Column
+  @ColumnDefault("0")
+  Integer quantity;
+
+  @Column
+  @UpdateTimestamp
+  LocalDateTime updatedAt;
+
+  @Builder
+  public Cart(User user, Product product, int quantity) {
+    this.user = user;
+    this.product = product;
+    this.quantity = quantity;
+  }
+}
