dd4dc12469afb3644b49c1c029cc4ac0d9611034
myeong-han <ames11118@gmail.com>
2025-06-21 12:52:52 +0900

    add Entitiy (Cart)

---
+++ src/main/java/com/sparta/java2/project/part1/commerce/domain/cart/entity/Cart.java
@@ -0,0 +1,43 @@
+package com.sparta.java2.project.part1.commerce.domain.cart.entity;
+
+import com.sparta.java2.project.part1.commerce.common.entity.BaseTimeEntity;
+import com.sparta.java2.project.part1.commerce.domain.product.entity.Product;
+import com.sparta.java2.project.part1.commerce.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Table
+@DynamicInsert
+@DynamicUpdate
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Cart extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    Product product;
+
+    @Column(nullable = false)
+    Integer quantity;
+
+    @Builder
+    public Cart(User user, Product product, Integer quantity) {
+        this.user = user;
+        this.product = product;
+        this.quantity = quantity;
+    }
+}
---
+++ src/main/java/com/sparta/java2/project/part1/commerce/domain/cart/repository/CartRepository.java
@@ -0,0 +1,9 @@
+package com.sparta.java2.project.part1.commerce.domain.cart.repository;
+
+import com.sparta.java2.project.part1.commerce.domain.cart.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+}
