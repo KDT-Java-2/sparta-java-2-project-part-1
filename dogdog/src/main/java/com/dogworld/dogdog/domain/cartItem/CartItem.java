f1275edb94d942d51114f6c37f873fe9510e794b
techhan <devhan9117@gmail.com>
2025-06-21 15:14:54 +0900

    cartItem flyway, entity 작성

---
+++ dogdog/src/main/java/com/dogworld/dogdog/domain/cartItem/CartItem.java
@@ -0,0 +1,50 @@
+package com.dogworld.dogdog.domain.cartItem;
+
+import com.dogworld.dogdog.domain.BaseEntity;
+import com.dogworld.dogdog.domain.cart.Cart;
+import com.dogworld.dogdog.domain.product.Product;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.math.BigDecimal;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CartItem extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "cart_id", nullable = false)
+  private Cart cart;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "product_id", nullable = false)
+  private Product product;
+
+  @Column(nullable = false)
+  private int quantity;
+
+  @Column(nullable = false)
+  private BigDecimal unitPrice;
+
+  @Column(nullable = false)
+  private BigDecimal totalPrice;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private CartItemStatus status;
+}
