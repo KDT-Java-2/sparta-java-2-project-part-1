c6026fab25c5d9a24a6df834fe809a1be45cfc27
kadun1 <kadunjj@gmail.com>
2025-06-21 09:59:00 +0900

    inital commit

---
+++ src/main/java/com/sparta/spartajava2projectpart1/SpartaJava2ProjectPart1Application.java
@@ -0,0 +1,13 @@
+package com.sparta.spartajava2projectpart1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaJava2ProjectPart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpartaJava2ProjectPart1Application.class, args);
+	}
+
+}
