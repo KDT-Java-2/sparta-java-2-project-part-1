21837922c2db761be6b7c66c10d99b8dbaaec939
tommyheo <jyhuh8775@naver.com>
2025-06-21 11:34:48 +0900

    FEAT: 프로젝트 초기 세팅

---
+++ ecommer-project/src/test/java/com/tommy/ecommerproject/EcommerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tommy.ecommerproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
