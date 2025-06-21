414f4985341e0cd4aca22d6fe68562fbebccb3ee
chahyeonho <chamym@naver.com>
2025-06-21 09:59:50 +0900

    init commit

---
+++ src/test/java/me/chahyunho/projectweek1/ProjectWeek1ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.chahyunho.projectweek1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWeek1ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
