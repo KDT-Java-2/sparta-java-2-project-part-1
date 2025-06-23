14b792e918e640a78573c16087e83e9584acdbc6
JeongHoon.Han <yuria2005@gmail.com>
2025-06-23 23:21:40 +0900

    feature: 프로젝트 생성

---
+++ src/main/java/com/home/sparta_01_commerce/Sparta01CommerceApplication.java
@@ -0,0 +1,13 @@
+package com.home.sparta_01_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sparta01CommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sparta01CommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/home/sparta_01_commerce/Sparta01CommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.sparta_01_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sparta01CommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
