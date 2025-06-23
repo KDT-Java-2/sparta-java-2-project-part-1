7f1da70006fa1f05e894418f531eeee0c053a52e
박주현 <pjh365@cubecore.co.kr>
2025-06-23 23:10:19 +0900

    설정파일 수정

---
+++ java-02-project/src/main/java/com/sparta/dev/java_02_project/Java02ProjectApplication.java
@@ -0,0 +1,14 @@
+package com.sparta.dev.java_02_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java02ProjectApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run 이게 돌아가야 스프링부트가 실행됨
+		SpringApplication.run(Java02ProjectApplication.class, args);
+	}
+
+}
---
+++ java-02-project/src/test/java/com/sparta/dev/java_02_project/Java02ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.dev.java_02_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java02ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
