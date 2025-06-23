65f4b16209429862ea53031903bc579c7a3cf6ef
move-wook <ldu0215@gmail.com>
2025-06-23 14:11:32 +0900

    환불 연관관계 수정

--- src/main/java/com/moveuk/ecommerce/domain/refund/Refund.java
+++ src/main/java/com/moveuk/ecommerce/domain/refund/Refund.java
@@ -22,14 +22,6 @@ public class Refund {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "purchase_id", nullable = false)
-    private Purchase purchase;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
-
-    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "payment_id", nullable = false)
     private Payment payment;
 
