e4582aa6e737633cfa9f94ef149ed3f9d2acf1c6
JB <whdqja918@gmail.com>
2025-06-28 17:31:15 +0900

    프로젝트 초기세팅

---
+++ prj_part_1/src/main/java/org/example/prj_part_1/common/enums/RefundStatus.java
@@ -0,0 +1,4 @@
+package org.example.prj_part_1.common.enums;
+
+public enum RefundStatus {
+}
---
+++ prj_part_1/src/test/java/org/example/prj_part_1/PrjPart1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.prj_part_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjPart1ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
