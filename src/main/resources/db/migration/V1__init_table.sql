84a3c7244f35fdc4dcc13262e8a0ad4753018f74
JeongHoon.Han <yuria2005@gmail.com>
2025-06-30 23:16:52 +0900

    feature: Flyway create table
    1. Flyway 마이그레이션 파일들:
     - 총 8개(`user`, `category`, `product`, `purchase`, `purchase_product`, `cart`, `refund`) 테이블에 대한 `CREATE TABLE` SQL 스크립트.

---
+++ src/main/resources/db/migration/V1__init_table.sql
@@ -0,0 +1,81 @@
+CREATE TABLE user (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+ALTER TABLE `user` ADD updatedAt datetime DEFAULT CURRENT_TIMESTAMP  NULL;
+ALTER TABLE `user` ADD status varchar(10) DEFAULT 'ACTIVE' NULL ;
+
+-- purchase 테이블
+CREATE TABLE purchase (
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  user_id BIGINT NOT NULL, -- FK: 어떤 user의 주문인지 식별
+  total_price DECIMAL(10, 2) NOT NULL,
+  status VARCHAR(20) NOT NULL,
+  created_at DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6)
+);
+
+
+-- product Table
+CREATE TABLE product (
+ id BIGINT AUTO_INCREMENT PRIMARY KEY,
+ name VARCHAR(255) NOT NULL,
+ description TEXT,
+ price DECIMAL(10, 2) NOT NULL,
+ stock INT NOT NULL DEFAULT 0,
+ category_id BIGINT COMMENT '상품이 속한 카테고리 ID',
+ created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+ updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- 연결 테이블: `purchase_item`
+CREATE TABLE purchase_product (
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  purchase_id BIGINT NOT NULL,   -- FK: 어떤 주문에 속하는지
+  product_id BIGINT NOT NULL,    -- FK: 어떤 상품인지
+  quantity INT NOT NULL,         -- 주문 수량
+  price DECIMAL(10, 2) NOT NULL,  -- 주문 시점의 상품 가격
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE category (
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  parent_id BIGINT DEFAULT NULL, -- FK: 부모 카테고리의 id를 참조
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE cart
+(
+    id         BIGINT AUTO_INCREMENT NOT NULL,
+    user_id    BIGINT                NOT NULL,
+    product_id BIGINT                NOT NULL,
+    quantity   INT                   NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT pk_cart PRIMARY KEY (id)
+);
+
+ALTER TABLE cart ADD CONSTRAINT FK_CART_ON_PRODUCT FOREIGN KEY (product_id) REFERENCES product (id);
+
+ALTER TABLE cart ADD CONSTRAINT FK_CART_ON_USER FOREIGN KEY (user_id) REFERENCES user (id);
+
+
+CREATE TABLE refund
+(
+    id          BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY ,
+    purchase_id BIGINT                NOT NULL,
+    reason      VARCHAR(255)          NULL,
+    status      VARCHAR(255)          NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+
+ALTER TABLE refund ADD CONSTRAINT FK_REFUND_ON_PURCHASE FOREIGN KEY (purchase_id) REFERENCES purchase (id);
