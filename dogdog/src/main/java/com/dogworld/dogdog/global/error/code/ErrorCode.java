cc25500712d1b544f6981f82cd7987c56044404a
techhan <devhan9117@gmail.com>
2025-07-01 23:28:28 +0900

    ErrorCode 구조 리팩토링 및 NOT_FOUND_PRODUCT 추가

--- dogdog/src/main/java/com/dogworld/dogdog/global/error/code/ErrorCode.java
+++ dogdog/src/main/java/com/dogworld/dogdog/global/error/code/ErrorCode.java
@@ -2,23 +2,28 @@ package com.dogworld.dogdog.global.error.code;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Getter
-@AllArgsConstructor
+@RequiredArgsConstructor
 public enum ErrorCode {
-    NOT_FOUND_USER("NOT_FOUND_USER", "존재하지 않는 멤버입니다.", HttpStatus.NOT_FOUND),
-    DUPLICATED_EMAIL("DUPLICATED_EMAIL", "이미 사용 중인 이메일입니다.", HttpStatus.CONFLICT),
-    DUPLICATED_USERNAME("DUPLICATED_USERNAME", "이미 사용 중인 아이디입니다.", HttpStatus.CONFLICT),
 
+    // == Member ==
+    NOT_FOUND_USER("존재하지 않는 멤버입니다.", HttpStatus.NOT_FOUND),
+    DUPLICATED_EMAIL("이미 사용 중인 이메일입니다.", HttpStatus.CONFLICT),
+    DUPLICATED_USERNAME("이미 사용 중인 아이디입니다.", HttpStatus.CONFLICT),
 
-    //category
-    NOT_FOUND_CATEGORY("NOT_FOUND_CATEGORY", "카테고리를 찾을 수 없습니다.", HttpStatus.NOT_FOUND)
-    ;
+    // == Product ==
+    NOT_FOUND_PRODUCT("존재하지 않는 상품입니다.", HttpStatus.NOT_FOUND),
 
+    // == Category ==
+    NOT_FOUND_CATEGORY("카테고리를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
 
-    private final String code;
     private final String message;
     private final HttpStatus httpStatus;
 
+    public String getCode() {
+        return name();
+    }
 }
