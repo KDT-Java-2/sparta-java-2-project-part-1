72077ffeede5bcd1e21a20e36d07423ef88f049f
move-wook <ldu0215@gmail.com>
2025-07-07 22:12:40 +0900

    에러코드 추가

--- src/main/java/com/moveuk/ecommerce/support/EcommerceErrorCode.java
+++ src/main/java/com/moveuk/ecommerce/support/EcommerceErrorCode.java
@@ -6,7 +6,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum EcommerceErrorCode {
 
-    NOT_FOUND_USER("200", "사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+    NOT_FOUND_USER("200", "사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    NOT_FOUND_PRODUCT("200", "상품을 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
 
     private final String code;
     private final String message;
