2f86ef0b6298f418cb44d7df4adfcdefcbd28f33
chaenchaen99 <chaenjung.99@gmail.com>
2025-07-10 21:14:50 +0900

    1주차 과제

---
+++ src/main/java/com/example/chaenchaen/common/enums/PurchaseStatus.java
@@ -0,0 +1,7 @@
+package com.example.chaenchaen.common.enums;
+
+public enum PurchaseStatus {
+  PENDING,
+  COMPLETED,
+  CANCELLED,
+}
---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,46 @@
+CREATE TABLE users (
+                       id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                       username VARCHAR(50) NOT NULL,
+                       email VARCHAR(255) NOT NULL UNIQUE,
+                       password_hash VARCHAR(255) NOT NULL,
+                       created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                       updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE category (
+                          id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                          name VARCHAR(255) NOT NULL,
+                          parent_id BIGINT DEFAULT NULL COMMENT '부모 카테고리 ID (자기 참조)',
+                          created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                          updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE product (
+                         id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                         category_id BIGINT NOT NULL,
+                         name VARCHAR(255) NOT NULL,
+                         description TEXT,
+                         price DECIMAL(10, 2) NOT NULL,
+                         stock INT NOT NULL,
+                         created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                         updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE purchase (
+                          id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                          user_id BIGINT NOT NULL, -- FK: 어떤 user의 주문인지 식별
+                          total_price DECIMAL(10, 2) NOT NULL,
+                          status VARCHAR(20) NOT NULL,
+                          created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                          updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE purchase_product ( -- 단수형으로 이름 변경
+                                  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                  purchase_id BIGINT NOT NULL COMMENT '어떤 주문에 속하는지',
+                                  product_id BIGINT NOT NULL COMMENT '어떤 상품인지',
+                                  quantity INT NOT NULL,
+                                  price DECIMAL(10, 2) NOT NULL COMMENT '주문 시점의 상품 가격',
+                                  created_at DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
+                                  updated_at DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6)
+);
