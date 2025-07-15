6acd682dffb1413fe0b5cc4f3f5788dfca58a4d3
BoxxHam <sangja234@gmail.com>
2025-07-15 20:54:42 +0900

    🚀 chore: 공통 응답, 예외 설정

---
+++ src/main/java/com/scb/masterCourse/commerce/common/exception/ServiceExceptionCode.java
@@ -0,0 +1,17 @@
+package com.scb.masterCourse.commerce.common.exception;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public enum ServiceExceptionCode {
+
+    NOT_FOUND_USER("사용자를 찾을 수 없습니다."),
+    ;
+
+    final String message;
+}
