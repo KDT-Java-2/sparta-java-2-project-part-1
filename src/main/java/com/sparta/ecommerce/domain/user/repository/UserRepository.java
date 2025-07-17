2066753b0d3e2493f1f67f6648908391fb2bdf0c
SeongCheolKwon <kwon0220@gmail.com>
2025-07-17 21:18:06 +0900

    [사용자] 회원가입 API 추가

--- src/main/java/com/sparta/ecommerce/domain/user/repository/UserRepository.java
+++ src/main/java/com/sparta/ecommerce/domain/user/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.sparta.ecommerce.domain.user.repository;
 
 import com.sparta.ecommerce.domain.user.entity.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+  List<User> findByEmail(String email);
 }
