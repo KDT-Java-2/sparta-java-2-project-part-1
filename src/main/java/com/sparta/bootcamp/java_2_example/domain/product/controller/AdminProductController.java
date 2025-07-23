dfd7d36396f5a0058f2aa90aaceb2aa144a34fb7
CHO-YOUNGSEO <ozlorv@naver.com>
2025-07-23 23:38:11 +0900

    feat: 어드민 상품 수정

--- src/main/java/com/sparta/bootcamp/java_2_example/domain/product/controller/AdminProductController.java
+++ src/main/java/com/sparta/bootcamp/java_2_example/domain/product/controller/AdminProductController.java
@@ -3,13 +3,12 @@ package com.sparta.bootcamp.java_2_example.domain.product.controller;
 import com.sparta.bootcamp.java_2_example.common.response.ApiResponse;
 import com.sparta.bootcamp.java_2_example.domain.product.dto.ProductCreateRequest;
 import com.sparta.bootcamp.java_2_example.domain.product.dto.ProductCreateResponse;
+import com.sparta.bootcamp.java_2_example.domain.product.dto.ProductResponse;
+import com.sparta.bootcamp.java_2_example.domain.product.dto.ProductUpdateRequest;
 import com.sparta.bootcamp.java_2_example.domain.product.service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,6 +22,14 @@ public class AdminProductController {
             @Valid @RequestBody ProductCreateRequest request
     ) {
         return ApiResponse.success(productService.save(request));
+    }
+
+    @PutMapping("/{productId}")
+    public ApiResponse<ProductResponse> update(
+            @PathVariable Long productId,
+            @Valid @RequestBody ProductUpdateRequest request
+    ) {
+        return ApiResponse.success(productService.update(productId, request));
     }
 
 }
--- src/main/java/com/sparta/bootcamp/java_2_example/domain/product/service/ProductService.java
+++ src/main/java/com/sparta/bootcamp/java_2_example/domain/product/service/ProductService.java
@@ -2,18 +2,21 @@ package com.sparta.bootcamp.java_2_example.domain.product.service;
 
 import com.sparta.bootcamp.java_2_example.common.exception.ServiceException;
 import com.sparta.bootcamp.java_2_example.common.exception.ServiceExceptionCode;
+import com.sparta.bootcamp.java_2_example.domain.category.entity.Category;
 import com.sparta.bootcamp.java_2_example.domain.category.repository.CategoryRepository;
 import com.sparta.bootcamp.java_2_example.domain.product.dto.*;
 import com.sparta.bootcamp.java_2_example.domain.product.entity.Product;
 import com.sparta.bootcamp.java_2_example.domain.product.mapper.ProductMapper;
 import com.sparta.bootcamp.java_2_example.domain.product.repository.ProductQueryRepository;
 import com.sparta.bootcamp.java_2_example.domain.product.repository.ProductRepository;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -39,15 +42,45 @@ public class ProductService {
 
     @Transactional
     public ProductCreateResponse save(ProductCreateRequest request) {
-        if (!categoryRepository.existsById(request.getCategoryId())) {
-            throw new ServiceException(ServiceExceptionCode.NOT_FOUND_CATEGORY);
-        }
+        ensureCategoryExists(request.getCategoryId());
 
-        if (productRepository.existsByName(request.getName())) {
-            throw new ServiceException(ServiceExceptionCode.ALREADY_EXISTS_PRODUCT_NAME);
-        }
+        ensureNameUnique(request.getName());
 
         Product product = productRepository.save(productMapper.toEntity(request));
         return productMapper.toCreateResponse(product);
+    }
+
+    @Transactional
+    public ProductResponse update(Long productId, ProductUpdateRequest request) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new ServiceException(ServiceExceptionCode.NOT_FOUND_PRODUCT));
+
+        if (!product.getName().equals(request.getName())) {
+            ensureNameUnique(request.getName());
+        }
+
+        Category category = categoryRepository.findById(request.getCategoryId())
+                .orElseThrow(() -> new ServiceException(ServiceExceptionCode.NOT_FOUND_CATEGORY));
+
+        product.setCategory(category);
+        product.setName(request.getName());
+        product.setDescription(request.getDescription());
+        product.setPrice(BigDecimal.valueOf(request.getPrice()));
+        product.setStock(request.getStock());
+        product.setUpdatedAt(LocalDateTime.now());
+
+        return productMapper.toResponse(product);
+    }
+
+    private void ensureCategoryExists(Long categoryId) {
+        if (!categoryRepository.existsById(categoryId)) {
+            throw new ServiceException(ServiceExceptionCode.NOT_FOUND_CATEGORY);
+        }
+    }
+
+    private void ensureNameUnique(String name) {
+        if (productRepository.existsByName(name)) {
+            throw new ServiceException(ServiceExceptionCode.ALREADY_EXISTS_PRODUCT_NAME);
+        }
     }
 }
