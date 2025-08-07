8c55d13a7f18a76df6fb44a9f27b139fc7f20a78
rlask <rlask55@naver.com>
2025-08-07 16:01:29 +0900

    2025 1주차 과제 커밋

---
+++ src/main/java/com/sparta/java2_project_part1/domain/user/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.sparta.java2_project_part1.domain.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+
+  private final
+
+}
---
+++ src/main/java/com/sparta/java2_project_part1/domain/user/service/UserService.java
@@ -0,0 +1,12 @@
+package com.sparta.java2_project_part1.domain.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+  private final
+
+}
