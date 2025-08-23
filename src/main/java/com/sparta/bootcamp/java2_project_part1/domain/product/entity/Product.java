5583829a4f550c0770fca92640f8c8551334ad3f
prolkh <prolkh@gmail.com>
2025-08-23 11:03:00 +0900

    feat: product 도메인 만들기

---
+++ src/main/java/com/sparta/bootcamp/java2_project_part1/domain/product/entity/Product.java
@@ -0,0 +1,67 @@
+package com.sparta.bootcamp.java2_project_part1.domain.product.entity;
+
+import com.sparta.bootcamp.java2_project_part1.common.enums.ProductStatus;
+import com.sparta.bootcamp.java2_project_part1.domain.category.entity.Category;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Table(name = "product")
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Product {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  Long id;
+
+  @Column(nullable = false, length = 50)
+  String name;
+
+  @Column
+  String description;
+
+  @Column(nullable = false)
+  BigDecimal sellPrice;
+
+  @Column(nullable = false)
+  BigDecimal buyPrice;
+
+  @Column
+  Integer stock;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "category_id", nullable = false)
+  Category category;
+
+  @Enumerated(EnumType.STRING)
+  @Column
+  ProductStatus status;
+
+  @CreationTimestamp
+  @Column(nullable = false, updatable = false)
+  LocalDateTime createdAt;
+
+  @Column
+  @UpdateTimestamp
+  LocalDateTime updatedAt;
+}
